bd6413438a0b8cb74e08068d2c8869ba35525593
JuanD <juand.herrera@hotmail.com>
2018-03-22 14:44:25 -0500

    Comentarios para la clase de UtilidadesFecha

--- src/main/java/dominio/UtilidadesFecha.java
+++ src/main/java/dominio/UtilidadesFecha.java
@@ -3,7 +3,25 @@ package dominio;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * CLASE UTILIDADES FECHA QUE CONTIENE MÉTODO PARA REALIZAR OPERACIONES CON
+ * FECHAS.
+ * 
+ * @author juan.herrera
+ *
+ */
 public class UtilidadesFecha {
+
+	/**
+	 * MÉTODO QUE RETONA UNA FECHA POSTERIOR A LA FECHA INGRESADA SI LA SUMA DE
+	 * LOS DIGITOS DEL ISBN ES MAYOR A 30, SE SUMARAN LOS DÍAS INGRESADOS POR
+	 * PARAMETRO A LA FECHA DE INICIO.
+	 * 
+	 * @param isbn
+	 * @param fechaPrestamo
+	 * @param cantidadDiasPrestamo
+	 * @return
+	 */
 	public static Date prestamoHasta(String isbn, Date fechaPrestamo, int cantidadDiasPrestamo) {
 		if (sumatoriaDigitosIsbn(isbn) > 30) {
 			fechaPrestamo = agregarDiasSinContarDomingos(fechaPrestamo, cantidadDiasPrestamo);
@@ -13,6 +31,12 @@ public class UtilidadesFecha {
 		return fechaPrestamo;
 	}
 
+	/**
+	 * MÉTODO QUE SUMA LOS DIGITOS DE UNA CADENA.
+	 * 
+	 * @param isbn
+	 * @return
+	 */
 	private static int sumatoriaDigitosIsbn(String isbn) {
 		int valorTotal = 0;
 		char[] arreglo = isbn.toCharArray();
@@ -24,6 +48,14 @@ public class UtilidadesFecha {
 		return valorTotal;
 	}
 
+	/**
+	 * MÉTODO QUE CUENTA UNA CANTIDAD DE DÍAS ESPECIFICADOS A UNA FECHA DE
+	 * INICIO, EVITANDO TENER EN CUENTA LOS DOMINGOS.
+	 * 
+	 * @param fechaPrestamo
+	 * @param cantidadDiasPrestamo
+	 * @return
+	 */
 	private static Date agregarDiasSinContarDomingos(Date fechaPrestamo, int cantidadDiasPrestamo) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(fechaPrestamo);
